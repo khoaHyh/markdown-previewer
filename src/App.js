273aaf4f7252bdc6891689a175e51ace70b7bb82
khoaHyh <khoahuynh.toronto@gmail.com>
2020-12-18 12:53:36 -0500

    Feat: Initialize app
    Add react-markdown, react-fcctest and initialize foundation for
    markdown-previewer.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ReactFCCtest from 'react-fcctest';
+import ReactMarkdown from 'react-markdown';
 
-function App() {
+const App = () => {
+  const markdownVariable = useState();
+  let markdown = markdownVariable[0];
+  let setMarkdown = markdownVariable[1];
+
+  useEffect(() => {
+    setMarkdown('hello world')
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ReactFCCtest />
+    <textarea id="editor" type="text"></textarea>
+    <div id="preview">
+      <ReactMarkdown children={markdown} />
+    </div>
     </div>
   );
 }
